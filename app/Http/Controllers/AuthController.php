122f5db564a7f64805994eff66d42e5f65932dd7
Nurfadhil12 <nuurrff@gmail.com>
2021-01-11 23:04:51 +0700

    add auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function me(){
+       return "NIS : 3103118104
+                Nama : Nur Fadhil Aminullah
+                Gender : laki laki
+                Phone : 081283927781
+                Kelas : XII RPL 3";
+    }
+}
